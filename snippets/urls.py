a773d1716bac93437fa5e39d97e7655fe1c0f729
root <root@ip-172-31-6-136.ap-southeast-1.compute.internal>
2015-01-20 06:18:20 +0000

    Add authentication, for apache->WSGI, need  in http configuration file

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views, classbased_views
 
@@ -7,10 +7,33 @@ from snippets import views, classbased_views
 #    url(r'(?P<pk>[0-9]+)/$', views.snippet_detail),
 #]
 
-urlpatterns = [
-    url(r'^$', classbased_views.SnippetList.as_view()),
-    url(r'(?P<pk>[0-9]+)/$', classbased_views.SnippetDetail.as_view()),
+#urlpatterns = [
+#    url(r'^$', classbased_views.SnippetList.as_view()),
+#    url(r'(?P<pk>[0-9]+)/$', classbased_views.SnippetDetail.as_view()),
+#    url(r'^users/$', classbased_views.UserList.as_view()),
+#    url(r'users/(?P<pk>[0-9]+)/$', classbased_views.UserDetail.as_view()),
+#]
+
+urlpatterns = format_suffix_patterns([
+        url(r'^api/$', 
+            classbased_views.api_root),
+        url(r'^$', 
+            classbased_views.SnippetList.as_view(), 
+            name = 'snippet-list'),
+        url(r'(?P<pk>[0-9]+)/$', 
+            classbased_views.SnippetDetail.as_view(), 
+            name = 'snippet-detail'),
+        url(r'^users/$', 
+            classbased_views.UserList.as_view(), 
+            name = 'user-list'),
+        url(r'users/(?P<pk>[0-9]+)/$', 
+            classbased_views.UserDetail.as_view(), 
+            name = 'user-detail'),
+])
+
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
 ]
-
-
-urlpatterns = format_suffix_patterns(urlpatterns)
